fa8d433b138552ffbf7408ebb5c9bc9e6ff93122
Chris Davison <chris@cdwmbp.local>
2014-08-28 13:44:53 -0700

    Updated media keys userscript to work with Rhapsody's new beta site.

--- src/mediaKeysPlugin.js
+++ src/mediaKeysPlugin.js
@@ -1,18 +1,48 @@
 mediaKeysPlugin = {};
 (function() {
-  if (window.fluid) {
-    mediaKeysPlugin.forward = function() {
-      $("#player-next").click();
+  var play, next, previous;
+  if (Rhapsody) {
+    play = function () {
+      if (Rhapsody.queueController.isPlaying) {
+        Rhapsody.queueController.pause();
+      } else {
+        Rhapsody.queueController.resume();
+      }
     };
-    mediaKeysPlugin.backward = function() {
-      $("#player-previous").click();
+    next = function () {
+      if (Rhapsody.queueController.isPlaying) {
+        Rhapsody.queueController.playNext();
+      }
     };
-    mediaKeysPlugin.play = function() {
+    previous = function () {
+      if (Rhapsody.queueController.isPlaying) {
+        Rhapsody.queueController.playPrevious();
+      }
+    };
+  } else {
+    play = function () {
       if ("none" == $("#player-play").css("display")) {
         $("#player-pause").click();
       } else {
         $("#player-play").click();
       }
+    };
+    next = function () {
+      $("#player-next").click();
+    };
+    previous = function () {
+      $("#player-previous").click();
+    };
+  }
+  if (window.fluid) {
+    mediaKeysPlugin.forward = function() {
+      next();
+    };
+    mediaKeysPlugin.backward = function() {
+      previous();
+    };
+    mediaKeysPlugin.play = function() {
+      play();
     };
   }
 })();
